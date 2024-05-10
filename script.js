a447993aba583a7fdb027cefa57fbbd0a0cb532c
taim-ayman <163901615+taim-ayman@users.noreply.github.com>
2024-05-10 13:25:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const before_loading = 
+	document.getElementById("content-before-loading"); 
+before_loading.style.display = "block"; 
+let c = 0; 
+function getInformation() { 
+	setTimeout(() => { 
+		for ( let i = 0;i < 100; i++ ) { 
+			const new_div = document.createElement("div"); 
+			new_div.className = "sub-block"; 
+			new_div.innerHTML = `taim is cool!!! ${c}${i}`; 
+			before_loading.appendChild(new_div); 
+		} 
+		c++; 
+	}, 1000); 
+} 
+
+window.addEventListener("scroll", () => { 
+	if ( 
+		document.documentElement.scrollTop + 
+		document.documentElement.clientHeight <=
+		document.documentElement.scrollHeight 
+	) { 
+		getInformation(); 
+	} 
+}); 
+getInformation();
+
